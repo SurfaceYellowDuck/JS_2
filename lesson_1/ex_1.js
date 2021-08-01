b63361a22849ef1c5f44af893f40173b010045c3
Nikita <lukonenko.nikitka@mail.ru>
2021-08-01 09:40:02 +0300

    v1.1 commit

--- lesson_1/ex_1.js
+++ lesson_1/ex_1.js
@@ -10,9 +10,14 @@ const goods = [
 const renderGoodsItem = (title = 'standart product', price = 0) => `<div class="goods-item"><h3>${title}</h3><p class='good-price'>${price}</p></div>`;
 const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    console.log(goodsList)
-    document.querySelector('.goods-list').innerHTML = goodsList;
-}
+    let goods_items = document.querySelector('.goods-list')
+    for (el of goodsList) {                                  //Первый вариант избавиться от запятых - поместить всё в цикл. Всё дело в том, 
+        goods_items.insertAdjacentHTML('afterbegin', el)     // что когда массив преобразуется в строку методы innerHTML и insertAdjacentHTML добавляют
+    }                                                        // запятую после каждого элемента массива.                           
+
+    goods_items.innerHTML = goodsList.join(' ')             //Второй вариант избавиться от запятых - можно добавить метод join, который заменит все
+}                                                            //запятые на пробелы.    
+
 
 window.onload = () => renderGoodsList(goods);
 
