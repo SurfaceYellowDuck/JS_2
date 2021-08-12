f94d514144ebdaa5ea2247075ea584767d45c724
Nikita <lukonenko.nikitka@mail.ru>
2021-08-12 18:53:10 +0300

    v4.0 commit

--- lesson_1/js.js
+++ lesson_1/js.js
@@ -107,104 +107,112 @@ Productlist.fetchGoods()
 const basket = new BasketList();
 // basketList.appendItem();
 
-window.onload = () => {  // При клике по кнопке корзина рендерятся её элементы, стоимость и количество элементов
-    Productlist.renderProductList()
-    $(".cart-button").click(function () {
-        basket.renderBasketItems();
-        basket.renderBasketInfo();
-    });
-    function PushItemToBasket() {
-        $(".catalog-goods-item").click(function (event) {
-            let targetName = event.target.className
-            console.log(targetName)
-            if (targetName.includes('append')) {
-                // console.log(target_name)
-                itemName = targetName.split(' ')[1];
-                bigFor: for (productItem of Productlist.goodlist) {
-                    if (productItem.title == itemName) {
-                        if (basket.basketList.length == 0) {
-                            productItem.quantity = 1
-                            basket.basketList.push(productItem)
-                            console.log(basket.basketList)
-                        }
-                        else {
-                            for (basketItem of basket.basketList) {
-                                if (productItem.title === basketItem.title) {
-                                    basketItem.quantity += 1
-                                    console.log(basket.basketList)
-                                    break bigFor;
-                                }
+// window.onload = () => {  // При клике по кнопке корзина рендерятся её элементы, стоимость и количество элементов
+//     Productlist.renderProductList()
+//     $(".cart-button").click(function () {
+//         basket.renderBasketItems();
+//         basket.renderBasketInfo();
+//     });
+//     function PushItemToBasket() {
+//         $(".catalog-goods-item").click(function (event) {
+//             let targetName = event.target.className
+//             console.log(targetName)
+//             if (targetName.includes('append')) {
+//                 // console.log(target_name)
+//                 itemName = targetName.split(' ')[1];
+//                 bigFor: for (productItem of Productlist.goodlist) {
+//                     if (productItem.title == itemName) {
+//                         if (basket.basketList.length == 0) {
+//                             productItem.quantity = 1
+//                             basket.basketList.push(productItem)
+//                             console.log(basket.basketList)
+//                         }
+//                         else {
+//                             for (basketItem of basket.basketList) {
+//                                 if (productItem.title === basketItem.title) {
+//                                     basketItem.quantity += 1
+//                                     console.log(basket.basketList)
+//                                     break bigFor;
+//                                 }
 
-                            }
-                            productItem.quantity = 1
-                            basket.basketList.push(productItem)
-                            console.log(basket.basketList)
-                            // break bigFor;
-                        }
+//                             }
+//                             productItem.quantity = 1
+//                             basket.basketList.push(productItem)
+//                             console.log(basket.basketList)
+//                             // break bigFor;
+//                         }
 
-                    }
-                }
-            }
-        })
-    }
-    function DeleteItemFromBasket() {
-        $(".basket-items").click(function (event) {
-            let targetName = event.target.className
-            console.log(targetName)
-            if (targetName.includes('delete')) {
-                // console.log(target_name)
-                itemName = targetName.split(' ')[1];
-                basket.basketList.forEach((productItem, indexItem) => {
-                    if (productItem.title == itemName) {
-                        basket.basketList.splice(indexItem, 1)
-                        console.log(basket.basketList)
-                    }
-                })
-            }
-        })
-    }
-    PushItemToBasket()
-    DeleteItemFromBasket();
-};
+//                     }
+//                 }
+//             }
+//         })
+//     }
+//     function DeleteItemFromBasket() {
+//         $(".basket-items").click(function (event) {
+//             let targetName = event.target.className
+//             console.log(targetName)
+//             if (targetName.includes('delete')) {
+//                 // console.log(target_name)
+//                 itemName = targetName.split(' ')[1];
+//                 basket.basketList.forEach((productItem, indexItem) => {
+//                     if (productItem.title == itemName) {
+//                         basket.basketList.splice(indexItem, 1)
+//                         console.log(basket.basketList)
+//                     }
+//                 })
+//             }
+//         })
+//     }
+//     PushItemToBasket()
+//     DeleteItemFromBasket();
+// };
 
 
-let makeGETRequestPromice = (url) => {
-    return new Promise((success, error) => {
-        let xhr;
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-            xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xhr.onreadystatechange = () => {
-            setTimeout(() => {
-                if (xhr.readyState === 4) {
-                    success(xhr)
-                }
-                else if (xhr.status == 404) {
-                    error('resourse not found')
-                }
-                else if (xhr.status == 500) {
-                    error('error')
-                }
-            }, 250)
+// let makeGETRequestPromice = (url) => {
+//     return new Promise((success, error) => {
+//         let xhr;
+//         if (window.XMLHttpRequest) {
+//             xhr = new XMLHttpRequest();
+//         } else if (window.ActiveXObject) {
+//             xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//         }
+//         xhr.onreadystatechange = () => {
+//             setTimeout(() => {
+//                 if (xhr.readyState === 4) {
+//                     success(xhr)
+//                 }
+//                 else if (xhr.status == 404) {
+//                     error('resourse not found')
+//                 }
+//                 else if (xhr.status == 500) {
+//                     error('error')
+//                 }
+//             }, 250)
 
-        }
-        if (url) {
-            xhr.open('GET', url, true);
-            xhr.send()
-        }
-        else {
-            error('url does not exists')
-        }
-    })
-}
-let url = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+//         }
+//         if (url) {
+//             xhr.open('GET', url, true);
+//             xhr.send()
+//         }
+//         else {
+//             error('url does not exists')
+//         }
+//     })
+// }
+// let url = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-makeGETRequestPromice(`${url}/catalogData.json`)
-    .then((xhr) => {
-        console.log(xhr.responseText);
-    }),
-    (error) => {
-        console.log(error)
-    };
+// makeGETRequestPromice(`${url}/catalogData.json`)
+//     .then((xhr) => {
+//         console.log(xhr.responseText);
+//     }),
+//     (error) => {
+//         console.log(error)
+//     };
+
+
+
+let text = 'I told him \'Hello\'s\' little boys fSFewfw'
+
+console.log(text)
+
+console.log(text.replace(/^\'|\'(?=\W)|(?<=\W)'/g, '"'));
