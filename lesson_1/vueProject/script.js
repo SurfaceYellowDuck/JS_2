34f3c49723768c3da1473b02ecd2dfcb3a1b355e
Nikita <lukonenko.nikitka@mail.ru>
2021-08-25 00:07:29 +0300

    v.6.1 commit

--- lesson_1/vueProject/script.js
+++ lesson_1/vueProject/script.js
@@ -1,34 +1,98 @@
 Vue.component('goods-list', {
-    props: ['goods'],
+    props: ['goods', 'basket'],
     template: `
       <div class="goods-list">
-        <goods-item v-for="good in goods" :good="good"></goods-item>
+        <goods-item 
+        v-for="good in goods" 
+        :good="good" 
+        :key="good.product_name"
+        @addToCart="addToCart">
+        </goods-item>
       </div>
-    `
+    `,
+    methods: {
+        addToCart(data) {
+            for (el in this.basket) {
+                if (this.basket[el].product_name == data.product_name) {
+                    this.basket[el].quantity += 1
+                    console.log(this.basket[el].quantity)
+                    return
+                }
+            }
+            data.quantity = 1
+            this.basket.push(data)
+            console.log(this.basket)
+
+        }
+    }
 })
 
 Vue.component('goods-item', {
     props: ['good'],
-    template: `
-      <div class="goods-item">
+    template:
+        `
+      <div class="goods-item" >
         <h3>{{ good.product_name }}</h3>
         <p>{{ good.price }}</p>
-        <button class={{good.product_name}}>Добавить</button>
+        
+        <button 
+            @click='addToCart'>
+        Добавить</button>
       </div>
-    `
+    `,
+    methods: {
+        addToCart() {
+            this.$emit('addToCart', this.good)
+        }
+    }
 });
 
-Vue.component('basket-items', {
-    props: ['goods'],
-    data() {
-        return {
-            basket: []
-        }
-    },
-    methods: {
+Vue.component('basket-list', {
+    props: ['goods', 'basket'],
+    template: `
+      <div class="basket-list">
+        <h3>Корзина</h3>
+        <basket-item 
+            v-for="good in basket" 
+            :good="good"
+            :key="good.product_name"
+            @deleteFromCart="deleteFromCart">
+        </basket-item>
 
+      </div>
+    `,
+    methods: {
+        deleteFromCart(data) {
+            for (el in this.basket) {
+                if (this.basket[el].product_name == data.product_name) {
+                    this.basket.splice(el, 1)
+                    console.log(this.basket)
+                    break
+                }
+            }
+        }
     }
 })
+
+Vue.component('basket-item', {
+    props: ['good'],
+    template: `
+      <div class="basket-item">
+        <h3>{{ good.product_name }}</h3>
+        <p>{{ good.price }}<br>{{good.quantity}}
+        </p>
+        <p></p>
+        <button class="basket-delete-button" @click='deleteItemFromCart'>Удалить</button>
+      </div>
+    `,
+    methods: {
+        deleteItemFromCart() {
+            // console.log((this.good))
+            this.$emit('deleteFromCart', this.good)
+            // console.log(this.good)
+        }
+    }
+});
 
 Vue.component('goods-finder', {
     props: ['goods'],
@@ -55,7 +119,8 @@ Vue.component('goods-finder', {
     },
     template:
         `
-      <div class="find-item" v-on:find-item='find(searchLine)'>
+      <div class="find-item" 
+      v-on:find-item='find(searchLine)'>
         <input v-model="searchLine">
         
         <button @click="find(searchLine)">Найти</button>
@@ -83,8 +148,7 @@ const app = new Vue({
     data: {
         goods: [],
         filteredGoods: [],
-        // goodsStatusView: 1,
-        // searchLine: '',
+        basket: [],
     },
     methods: {
         makeGETRequest(url, callback) {
